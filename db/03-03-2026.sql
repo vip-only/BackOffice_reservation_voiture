341d2119f6a720bee7255876eb5c95085d91f472
IotyRandrianoely <ioty.ratisbonne@gmail.com>
2026-03-03 16:24:10 +0300

    feat-sql-asignation-vehivule

---
+++ db/03-03-2026.sql
@@ -0,0 +1,58 @@
+-- =========================================
+-- SPRINT 3 - CREATION DES TABLES
+-- =========================================
+
+DROP TABLE IF EXISTS distance CASCADE;
+DROP TABLE IF EXISTS aeroport CASCADE;
+DROP TABLE IF EXISTS parametre CASCADE;
+
+
+-- =========================================
+-- TABLE PARAMETRE (AVEC ID)
+-- =========================================
+
+CREATE TABLE parametre (
+    id SERIAL PRIMARY KEY,
+    cle VARCHAR(100) UNIQUE NOT NULL,
+    valeur NUMERIC(10,2) NOT NULL,
+    unite VARCHAR(50)
+);
+
+
+-- =========================================
+-- TABLE AEROPORT
+-- =========================================
+
+CREATE TABLE aeroport (
+    id SERIAL PRIMARY KEY,
+    code VARCHAR(10) UNIQUE NOT NULL,
+    libelle VARCHAR(150) NOT NULL
+);
+
+
+CREATE TABLE distance (
+    id SERIAL PRIMARY KEY,
+
+    from_id VARCHAR(20),
+    to_id VARCHAR(20),
+
+    kilometer NUMERIC(10,2) NOT NULL CHECK (kilometer >= 0)
+
+    -- Empêche qu’un aéroport ait une distance vers lui-même
+   
+);
+
+drop table reservation cascade;
+
+-- =========================
+CREATE TABLE reservation (
+    id SERIAL PRIMARY KEY,
+    client VARCHAR(200) NOT NULL,
+    nombre_passager  INTEGER,
+    date_heure_arrivee TIMESTAMP NOT NULL,
+    id_hotel INTEGER NOT NULL,
+    id_vehicule INTEGER ,
+    -- CONSTRAINT fk_client FOREIGN KEY (id_client) REFERENCES client(id_client),
+    CONSTRAINT fk_hotel FOREIGN KEY (id_hotel) REFERENCES hotel(id_hotel),
+    CONSTRAINT fk_vehicule FOREIGN KEY (id_vehicule) REFERENCES vehicule(id)
+);
